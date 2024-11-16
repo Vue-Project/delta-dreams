2055a5ce12bf464c873f9db05c2433d80a7e66bf
Ahmed Ayman <ahmed.ayman@systemira.net>
2024-11-16 11:02:36 +0200

    uploading all files liber in project

--- components/Reservation/CheckIn.vue
+++ components/Reservation/CheckIn.vue
@@ -3,15 +3,147 @@
     <h5 class="card-header"><i class="fa-solid fa-angle-left pr-2"></i>Add Reservation</h5>
     <hr class="m-0" />
     <div class="card-body">
-      <form id="formValidationExamples" class="row g-3 fv-plugins-bootstrap5 fv-plugins-framework"
+      <form id="formValidationReservation" class="row g-3 fv-plugins-bootstrap5 fv-plugins-framework"
         novalidate="novalidate">
-        <!-- Account Details -->
+        <!-- Reservation  Details -->
+        <div class="row">
+          <!-- Date Picker-->
+          <div class="col-md-6 col-12 mb-4">
+            <label for="flatpickr-date" class="form-label">Date Picker</label>
+            <input type="text" class="form-control flatpickr-input" placeholder="YYYY-MM-DD" id="flatpickr-date"
+              readonly="readonly">
+          </div>
+          <!-- /Date Picker -->
 
-        <div class="col-12">
-          <h6>1. Account Details</h6>
-          <hr class="mt-0">
+          <!-- Time Picker-->
+          <div class="col-md-6 col-12 mb-4">
+            <label for="flatpickr-time" class="form-label">Time Picker</label>
+            <input type="text" class="form-control flatpickr-input" placeholder="HH:MM" id="flatpickr-time"
+              readonly="readonly">
+          </div>
+          <!-- /Time Picker -->
+
+          <!-- Datetime Picker-->
+          <div class="col-md-6 col-12 mb-4">
+            <label for="flatpickr-datetime" class="form-label">Datetime Picker</label>
+            <input type="text" class="form-control flatpickr-input" placeholder="YYYY-MM-DD HH:MM"
+              id="flatpickr-datetime" readonly="readonly">
+          </div>
+          <!-- /Datetime Picker-->
+
+          <!-- Multiple Dates Picker-->
+          <div class="col-md-6 col-12 mb-4">
+            <label for="flatpickr-multi" class="form-label">Multiple Dates Picker</label>
+            <input type="text" class="form-control flatpickr-input" placeholder="YYYY-MM-DD HH:MM" id="flatpickr-multi"
+              readonly="readonly">
+          </div>
+          <!-- /Multiple Dates Picker-->
+
+          <!-- Range Picker-->
+          <div class="col-md-6 col-12 mb-4">
+            <label for="flatpickr-range" class="form-label">Range Picker</label>
+            <input type="text" class="form-control flatpickr-input" placeholder="YYYY-MM-DD to YYYY-MM-DD"
+              id="flatpickr-range" readonly="readonly">
+          </div>
+          <!-- /Range Picker-->
+
+          <!-- Human Friendly Date Picker-->
+          <div class="col-md-6 col-12 mb-4">
+            <label for="flatpickr-human-friendly" class="form-label">Human Friendly Date Picker</label>
+            <input type="hidden" class="form-control flatpickr-input" placeholder="Month DD, YYYY"
+              id="flatpickr-human-friendly"><input class="form-control form-control input" placeholder="Month DD, YYYY"
+              tabindex="0" type="text" readonly="readonly">
+          </div>
+          <!-- /Human Friendly Date Picker-->
+
+          <!-- Disabled Range-->
+          <div class="col-md-6 col-12 mb-md-0 mb-4">
+            <label for="flatpickr-disabled-range" class="form-label">Disabled Range</label>
+            <input type="text" class="form-control flatpickr-input" placeholder="YYYY-MM-DD"
+              id="flatpickr-disabled-range" readonly="readonly">
+          </div>
+          <!-- /Disabled Range-->
+
+          <!-- Inline Picker-->
+          <div class="col-md-6 col-12">
+            <label for="flatpickr-inline" class="form-label">Inline Picker</label>
+            <input type="text" class="form-control mb-1 flatpickr-input" placeholder="YYYY-MM-DD" id="flatpickr-inline"
+              readonly="readonly">
+            <div class="flatpickr-calendar animate inline" tabindex="-1">
+              <div class="flatpickr-months"><span class="flatpickr-prev-month"></span>
+                <div class="flatpickr-month">
+                  <div class="flatpickr-current-month"><span class="cur-month">November </span>
+                    <div class="numInputWrapper"><input class="numInput cur-year" type="number" tabindex="-1"
+                        aria-label="Year"><span class="arrowUp"></span><span class="arrowDown"></span></div>
+                  </div>
+                </div><span class="flatpickr-next-month"></span>
+              </div>
+              <div class="flatpickr-innerContainer">
+                <div class="flatpickr-rContainer">
+                  <div class="flatpickr-weekdays">
+                    <div class="flatpickr-weekdaycontainer">
+                      <span class="flatpickr-weekday">
+                        Sun</span><span class="flatpickr-weekday">Mon</span><span
+                        class="flatpickr-weekday">Tue</span><span class="flatpickr-weekday">Wed</span><span
+                        class="flatpickr-weekday">Thu</span><span class="flatpickr-weekday">Fri</span><span
+                        class="flatpickr-weekday">Sat
+                      </span>
+                    </div>
+                  </div>
+                  <div class="flatpickr-days" tabindex="-1">
+                    <div class="dayContainer"><span class="flatpickr-day prevMonthDay" aria-label="October 27, 2024"
+                        tabindex="-1">27</span><span class="flatpickr-day prevMonthDay" aria-label="October 28, 2024"
+                        tabindex="-1">28</span><span class="flatpickr-day prevMonthDay" aria-label="October 29, 2024"
+                        tabindex="-1">29</span><span class="flatpickr-day prevMonthDay" aria-label="October 30, 2024"
+                        tabindex="-1">30</span><span class="flatpickr-day prevMonthDay" aria-label="October 31, 2024"
+                        tabindex="-1">31</span><span class="flatpickr-day" aria-label="November 1, 2024"
+                        tabindex="-1">1</span><span class="flatpickr-day" aria-label="November 2, 2024"
+                        tabindex="-1">2</span><span class="flatpickr-day" aria-label="November 3, 2024"
+                        tabindex="-1">3</span><span class="flatpickr-day" aria-label="November 4, 2024"
+                        tabindex="-1">4</span><span class="flatpickr-day" aria-label="November 5, 2024"
+                        tabindex="-1">5</span><span class="flatpickr-day" aria-label="November 6, 2024"
+                        tabindex="-1">6</span><span class="flatpickr-day" aria-label="November 7, 2024"
+                        tabindex="-1">7</span><span class="flatpickr-day" aria-label="November 8, 2024"
+                        tabindex="-1">8</span><span class="flatpickr-day" aria-label="November 9, 2024"
+                        tabindex="-1">9</span><span class="flatpickr-day" aria-label="November 10, 2024"
+                        tabindex="-1">10</span><span class="flatpickr-day" aria-label="November 11, 2024"
+                        tabindex="-1">11</span><span class="flatpickr-day" aria-label="November 12, 2024"
+                        tabindex="-1">12</span><span class="flatpickr-day" aria-label="November 13, 2024"
+                        tabindex="-1">13</span><span class="flatpickr-day" aria-label="November 14, 2024"
+                        tabindex="-1">14</span><span class="flatpickr-day" aria-label="November 15, 2024"
+                        tabindex="-1">15</span><span class="flatpickr-day today" aria-label="November 16, 2024"
+                        aria-current="date" tabindex="-1">16</span><span class="flatpickr-day"
+                        aria-label="November 17, 2024" tabindex="-1">17</span><span class="flatpickr-day"
+                        aria-label="November 18, 2024" tabindex="-1">18</span><span class="flatpickr-day"
+                        aria-label="November 19, 2024" tabindex="-1">19</span><span class="flatpickr-day"
+                        aria-label="November 20, 2024" tabindex="-1">20</span><span class="flatpickr-day"
+                        aria-label="November 21, 2024" tabindex="-1">21</span><span class="flatpickr-day"
+                        aria-label="November 22, 2024" tabindex="-1">22</span><span class="flatpickr-day"
+                        aria-label="November 23, 2024" tabindex="-1">23</span><span class="flatpickr-day"
+                        aria-label="November 24, 2024" tabindex="-1">24</span><span class="flatpickr-day"
+                        aria-label="November 25, 2024" tabindex="-1">25</span><span class="flatpickr-day"
+                        aria-label="November 26, 2024" tabindex="-1">26</span><span class="flatpickr-day"
+                        aria-label="November 27, 2024" tabindex="-1">27</span><span class="flatpickr-day"
+                        aria-label="November 28, 2024" tabindex="-1">28</span><span class="flatpickr-day"
+                        aria-label="November 29, 2024" tabindex="-1">29</span><span class="flatpickr-day"
+                        aria-label="November 30, 2024" tabindex="-1">30</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 1, 2024" tabindex="-1">1</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 2, 2024" tabindex="-1">2</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 3, 2024" tabindex="-1">3</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 4, 2024" tabindex="-1">4</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 5, 2024" tabindex="-1">5</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 6, 2024" tabindex="-1">6</span><span class="flatpickr-day nextMonthDay"
+                        aria-label="December 7, 2024" tabindex="-1">7</span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Inline Picker-->
         </div>
 
+
+<!--
         <div class="col-md-6 fv-plugins-icon-container">
           <label class="form-label" for="formValidationName">Full Name</label>
           <input type="text" id="formValidationName" class="form-control" placeholder="John Doe"
@@ -23,9 +155,9 @@
           <input class="form-control" type="email" id="formValidationEmail" name="formValidationEmail"
             placeholder="john.doe">
           <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
-        </div>
+        </div> -->
 
-        <div class="col-md-6 fv-plugins-icon-container">
+        <!-- <div class="col-md-6 fv-plugins-icon-container">
           <div class="form-password-toggle">
             <label class="form-label" for="formValidationPass">Password</label>
             <div class="input-group input-group-merge has-validation">
@@ -48,11 +180,11 @@
             </div>
             <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
           </div>
-        </div>
+        </div> -->
 
         <!-- Personal Info -->
 
-        <div class="col-12">
+        <!-- <div class="col-12">
           <h6 class="mt-2">2. Personal Info</h6>
           <hr class="mt-0">
         </div>
@@ -186,7 +318,7 @@
 
         <!-- Choose Your Plan -->
 
-        <div class="col-12">
+        <!-- <div class="col-12">
           <h6 class="mt-2">3. Choose Your Plan</h6>
           <hr class="mt-0">
         </div>
@@ -252,7 +384,7 @@
         <div class="col-12">
           <button type="submit" name="submitButton" class="btn btn-primary waves-effect waves-light">Submit</button>
           <button type="submit" name="submitButton" class="btn btn-primary waves-effect waves-light">Submit</button>
-        </div>
+        </div> -->
         <input type="hidden">
       </form>
     </div>
