cba2705d0dac92779d0573ef18ad9a59c5ea4c4b
mo'men <nony.elfara@gmail.com>
2024-11-28 18:39:25 +0200

    fix the input style and switch

--- components/CalenderReservation/HeaderCalender.vue
+++ components/CalenderReservation/HeaderCalender.vue
@@ -8,7 +8,7 @@
         id="flatpickr-date-04"
         ref="datePicker4"
       />
-      <i class="fa-solid fa-calendar-days date-icon "></i>
+      <i class="fa-solid fa-calendar-days date-icon"></i>
     </div>
     <div class="col-md-6 col-12">
       <div class="d-flex items-center gap-4">
@@ -22,15 +22,16 @@
     </div>
     <div class="col-md-4 col-12">
       <div class="d-flex items-center gap-4">
-        <div class="mb-3">
-                        <select class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
-                          <option selected>The price is not included</option>
-                          <option value="1">The price includes breakfast</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                        </select>
-                      </div>
-
+        <div class="w-100">
+          <select
+            class="form-select"
+            id="exampleFormControlSelect1"
+            aria-label="Default select example"
+          >
+            <option selected>The price is not included</option>
+            <option value="1">The price includes breakfast</option>
+          </select>
+        </div>
         <label class="switch">
           <input type="checkbox" class="switch-input" v-model="isOn" />
           <span class="switch-toggle-slider">
@@ -41,7 +42,6 @@
           <span class="switch-label">{{ isOn ? 'Switch is ON' : 'Switch is OFF' }}</span> -->
         </label>
 
-
         <div @mouseenter="isHovered = true" @mouseleave="isHovered = false">
           <button type="button" class="btn btn-outline-primary waves-effect">
             <i class="fa-solid fa-circle-info"></i>
@@ -51,80 +51,83 @@
             v-show="isHovered"
             class="position-absolute left-0 mt-2 w-40 bg-white shadow-lg"
           >
-          <!-- start ul -->
-          <div class=" fullbox">
-                  <div class="row">
-                      <h5>Booking Status</h5>
-                      <hr>
-                      <div class="col-6">
-                          <ul class="Booking-Status-First">
-                          <li><i class="fas fa-square"></i>Arrived</li>
-                          <li><i class="fas fa-square"></i>Due Out</li>
-                          <li><i class="fas fa-square"></i>Maintenance Block</li>
-                          <li><i class="fas fa-square"></i>Dayuse Reservation</li>
-                          </ul>
-                      </div>
-                      <div class="col-6">
-                          <ul class="Booking-Status-Secound">
-                          <li><i class="fas fa-square"></i>Checked Out</li>
-                          <li><i class="fas fa-square"></i>Confirmed Reservation</li>
-                          <li><i class="fas fa-square"></i>Stayover</li>
-                          <li><i class="fas fa-square"></i>Dayuse</li>
-                          </ul>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <h5>Booking Indicators</h5>
-                      <hr>
-                      <div class="col-6">
-                          <ul class="Booking-Indicators-First">
-                          <li><i class="fa-solid fa-crown"></i>Group Owner</li>
-                          <li><i class="fa-solid fa-dollar-sign"></i>Payment Pending</li>
-                          <li><i class="fa-solid fa-user"></i>Single Lady</li>
-                          <li><i class="fa-solid fa-arrows-split-up-and-left"></i>Split Reservation</li>
-                          </ul>
-                      </div>
-                      <div class="col-6">
-                          <ul class="Booking-Indicators-Secound">
-                          <li><i class="fa-solid fa-user-group"></i>Group Booking</li>
-                          <li><i class="fa-solid fa-hand"></i>Stop Room Move</li>
-                          <li><i class="fa-solid fa-star"></i>Vip Guest</li>
-                          </ul>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <h5>Room Indicators</h5>
-                      <hr>
-                      <div class="col-6">
-                          <ul class="Room-Indicators-First">
-                          <li><i class="fa-solid fa-ban-smoking"></i>No Smoking</li>
-                          <li><i class="fa-solid fa-broom"></i>Dirty</li>
-                          <li><i class="fa-solid fa-clipboard-check"></i>Work Order</li>
-                          </ul>
-                      </div>
-                      <div class="col-6">
-                          <ul class="Room-Indicators-Secound">
-                          <li><i class="fa-solid fa-smoking"></i>Smoking</li>
-                          <li><i class="fa-solid fa-link"></i>Connected Rooms</li>
-                          </ul>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <hr>
-                      <div class="col-6">
-                          <ul class="Unclassified-First">
-                          <li><i class="fas fa-square"></i>Unassigned Room</li>
-                          <li><i class="fas fa-square"></i>Unconfirm Bookings</li>
-                          </ul>
-                      </div>
-                      <div class="col-6">
-                          <ul class="Unclassified-Secound">
-                          <li><i class="fas fa-square"></i>Inventory</li>
-                          </ul>
-                      </div>
-                  </div>
-          </div>
-          <!-- end ul -->
+            <!-- start ul -->
+            <div class="fullbox">
+              <div class="row">
+                <h5>Booking Status</h5>
+                <hr />
+                <div class="col-6">
+                  <ul class="Booking-Status-First">
+                    <li><i class="fas fa-square"></i>Arrived</li>
+                    <li><i class="fas fa-square"></i>Due Out</li>
+                    <li><i class="fas fa-square"></i>Maintenance Block</li>
+                    <li><i class="fas fa-square"></i>Dayuse Reservation</li>
+                  </ul>
+                </div>
+                <div class="col-6">
+                  <ul class="Booking-Status-Secound">
+                    <li><i class="fas fa-square"></i>Checked Out</li>
+                    <li><i class="fas fa-square"></i>Confirmed Reservation</li>
+                    <li><i class="fas fa-square"></i>Stayover</li>
+                    <li><i class="fas fa-square"></i>Dayuse</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row">
+                <h5>Booking Indicators</h5>
+                <hr />
+                <div class="col-6">
+                  <ul class="Booking-Indicators-First">
+                    <li><i class="fa-solid fa-crown"></i>Group Owner</li>
+                    <li><i class="fa-solid fa-dollar-sign"></i>Payment Pending</li>
+                    <li><i class="fa-solid fa-user"></i>Single Lady</li>
+                    <li>
+                      <i class="fa-solid fa-arrows-split-up-and-left"></i>Split
+                      Reservation
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-6">
+                  <ul class="Booking-Indicators-Secound">
+                    <li><i class="fa-solid fa-user-group"></i>Group Booking</li>
+                    <li><i class="fa-solid fa-hand"></i>Stop Room Move</li>
+                    <li><i class="fa-solid fa-star"></i>Vip Guest</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row">
+                <h5>Room Indicators</h5>
+                <hr />
+                <div class="col-6">
+                  <ul class="Room-Indicators-First">
+                    <li><i class="fa-solid fa-ban-smoking"></i>No Smoking</li>
+                    <li><i class="fa-solid fa-broom"></i>Dirty</li>
+                    <li><i class="fa-solid fa-clipboard-check"></i>Work Order</li>
+                  </ul>
+                </div>
+                <div class="col-6">
+                  <ul class="Room-Indicators-Secound">
+                    <li><i class="fa-solid fa-smoking"></i>Smoking</li>
+                    <li><i class="fa-solid fa-link"></i>Connected Rooms</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row">
+                <hr />
+                <div class="col-6">
+                  <ul class="Unclassified-First">
+                    <li><i class="fas fa-square"></i>Unassigned Room</li>
+                    <li><i class="fas fa-square"></i>Unconfirm Bookings</li>
+                  </ul>
+                </div>
+                <div class="col-6">
+                  <ul class="Unclassified-Secound">
+                    <li><i class="fas fa-square"></i>Inventory</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <!-- end ul -->
             <!-- out side box -->
             <div
               class="ant-popover ant-popover-placement-bottomRight ant-popover-hidden"
@@ -135,8 +138,7 @@
                   <span class="ant-popover-arrow-content"></span>
                 </div>
                 <div class="ant-popover-inner" role="tooltip">
-                  <div class="ant-popover-inner-content">
-                  </div>
+                  <div class="ant-popover-inner-content"></div>
                 </div>
               </div>
             </div>
@@ -199,6 +201,4 @@ export default {
   min-width: 24px;
   line-height: 24px;
 }
-
-
 </style>
