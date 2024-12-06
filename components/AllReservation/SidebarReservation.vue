708044f9d67c8da6319e9b2d59ad343b4e6b85d9
samamahfouz <samamahfoz43@gmail.com>
2024-12-06 17:26:34 +0200

    adding component

---
+++ components/AllReservation/SidebarReservation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <div v-if="isSidebarOpen" :class="['sidebar', { 'sidebar-open': isSidebarOpen }]" :style="{ width: width }">
+        <div class="sidebar-content">
+            <h3>{{ title }}</h3>
+            <hr class="my-2" />
+        </div>
+      </div>
+      <div v-if="isSidebarOpen" class="overlay" @click="$emit('close-sidebar')"></div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Sidebar",
+  layout: "component",
+  
+
+  props: {
+    isSidebarOpen: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: " Add Guest",
+    },
+    width: {
+      type: String,
+      default: "1000px",
+    },
+  },
+};
+</script>
---
+++ pages/reservations.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <HeaderReservation />
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+import HeaderReservation from "../components/ALLReservation/HeaderReservation.vue";
+
+
+export default {
+  name: "reservations",
+  layout: "main",
+  components: {
+    HeaderReservation,
+  },
+};
+</script>
+
+<style></style>
