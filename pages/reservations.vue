ebb8a5ef2f16818655e5bb58b52cd52e7c934e66
mo'men <nony.elfara@gmail.com>
2024-12-12 10:55:14 +0200

    .

--- pages/reservations.vue
+++ pages/reservations.vue
@@ -95,10 +95,6 @@
             >
               <i class="fa-solid fa-list"></i>
             </button>
-<<<<<<< HEAD
-
-            <button class="btn btn-primary waves-effect waves-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEnd" aria-controls="offcanvasEnd">
-=======
             <button
               type="button"
               class="ms-3 btn btn-primary dropdown-toggle waves-effect waves-light"
@@ -371,7 +367,6 @@
               data-bs-target="#offcanvasEnd"
               aria-controls="offcanvasEnd"
             >
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
               <i class="fs-6 me-1 fa-solid fa-magnifying-glass"></i>
               Search
             </button>
@@ -382,25 +377,6 @@
               aria-labelledby="offcanvasEndLabel"
             >
               <div class="offcanvas-header">
-<<<<<<< HEAD
-                <h5 id="offcanvasEndLabel" class="offcanvas-title">
-                  Offcanvas End
-                </h5>
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <hr />
-              <!-- <div class="row">
-                <div class="col-md-10 m-auto">
-                  <label for="MarketCode" class="col-form-label">Business Source</label>
-                  <select class="form-select mb-2" id="MarketCode">
-                    <option value="" disabled selected>-select-</option>
-                    <option value="option1">item</option>
-                    <option value="option2">item</option>
-                    <h3>geong3ng4iong</h3>
-                  </select>
-                </div>
-              </div> -->
-=======
                 <h5 id="offcanvasEndLabel" class="offcanvas-title">Search</h5>
                 <button
                   type="button"
@@ -410,38 +386,11 @@
                 ></button>
               </div>
               <hr class="m-0" />
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
 
               <!-- Dynamic Content Based on Active Tab -->
               <div v-if="activeTab === 'reservations'">
                 <div class="row">
                   <div class="col-md-10 m-auto">
-<<<<<<< HEAD
-                    <!-- Only show the heading if no card is selected -->
-                    <h1 v-if="!selectedCard">reservations</h1>
-
-                    <!-- Only display data if a card is selected -->
-                    <div v-if="selectedCard" class="mt-4">
-                      <h1>{{ selectedCard.guestName }}</h1>
-                      <p>Reservation No: {{ selectedCard.reservationNo }}</p>
-                      <p>
-                        Arrival: {{ selectedCard.arrivalDate }}
-                        {{ selectedCard.arrivalTime }}
-                      </p>
-                      <p>
-                        Departure: {{ selectedCard.departureDate }}
-                        {{ selectedCard.departureTime }}
-                      </p>
-                      <p>Room Details: {{ selectedCard.roomDetails }}</p>
-                      <p>Total: {{ selectedCard.total }} USD</p>
-                      <p>Paid: {{ selectedCard.paid }} USD</p>
-                      <p>Balance: {{ selectedCard.balance }} USD</p>
-                    </div>
-
-                    <!-- Default message when no card is selected -->
-                    <div v-else>
-                      <p>No Reservation Selected</p>
-=======
                     <div class="form-check form-check-primary mb-2">
                       <input
                         class="form-check-input input-filter"
@@ -573,7 +522,6 @@
                       <label class="form-check-label" for="Arrival">
                         Without Deposit
                       </label>
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
                     </div>
                   </div>
                 </div>
@@ -611,9 +559,6 @@
                       <option value="option1">item</option>
                       <option value="option2">item</option>
                     </select>
-<<<<<<< HEAD
-                    <h1>arrivals</h1>
-=======
                     <label for="MarketCode" class="col-form-label"
                       >Company</label
                     >
@@ -668,7 +613,6 @@
                         Guest Checked in Details
                       </label>
                     </div>
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
                   </div>
                 </div>
               </div>
@@ -756,9 +700,6 @@
                       <option value="option1">item</option>
                       <option value="option2">item</option>
                     </select>
-<<<<<<< HEAD
-                    <h1>inHouse</h1>
-=======
                     <label for="MarketCode" class="col-form-label"
                       >Travel Agent</label
                     >
@@ -795,7 +736,6 @@
                         </select>
                       </div>
                     </div>
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
                   </div>
                 </div>
               </div>
@@ -808,152 +748,6 @@
           </div>
         </template>
         <template #content>
-<<<<<<< HEAD
-          <div class="tab-pane fade active show" id="form-tabs-Reservations" role="tabpanel" v-if="activeTab === 'reservations'">
-            <div class="row">
-              <!-- Loop through reservations to display cards -->
-              <!-- <div class="col-md-4" v-for="card in reservations" :key="card.reservationNo">
-                <div class="card" style="cursor: pointer">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="card image" />
-                  <div class="card-body">
-                    <h5 class="card-title">{{ card.guestName }}</h5>
-                    <p class="card-text">
-                      Reservation No: {{ card.reservationNo }}
-                    </p>
-                    <p class="card-text">
-                      Arrival: {{ card.arrivalDate }} {{ card.arrivalTime }}
-                    </p>
-                    <p class="card-text">
-                      Departure: {{ card.departureDate }}
-                      {{ card.departureTime }}
-                    </p>
-                  </div>
-                </div>
-              </div> -->
-              <div v-if="viewMode === 'list'" class="table-responsive text-nowrap" @click="openOffcanvas(list)">
-                <table class="table" style="overflow: hidden;">
-                  <thead class="table-light">
-                    <tr>
-                      <th>Guest Name</th>
-                      <th>Res. No. Voucher No</th>
-                      <th>Arrival</th>
-                      <th>Departure</th>
-                      <th>Room Details</th>
-                      <th>Total ($)</th>
-                      <th>Paid ($)</th>
-                      <th>Balance ($)</th>
-                    </tr>
-                  </thead>
-                  <tbody class="table-border-bottom-0">
-                    <tr class="border-bottom" @click="openOffcanvas(list)">
-                      <td>
-                        <div class="d-flex align-items-center">
-                          <i class="text-primary fa-solid fa-hotel fs-3 mr-2 mb-2"></i>
-                          <div class="card-title mb-0">
-                            <h5 class="m-0 me-2">مصطفي مدبولي</h5>
-                            <div>
-                              <i class="fa-solid fa-person m-2"></i>3
-                              <i class="fa-solid fa-child m-2"></i>2
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>14527</td>
-                      <td>
-                        <div class="text-dark">02/12/2024</div>
-                        <div>08:43:01 pm</div>
-                      </td>
-                      <td>
-                        <div class="text-dark">02/12/2024</div>
-                        <div>08:43:01 pm</div>
-                      </td>
-                      <td>202 - Suite الفندق غرفتين وصاله السعر غير شامل</td>
-                      <td>$230</td>
-                      <td>$250</td>
-                      <td class="text-danger">$20</td>
-                      <td>
-                        <div class="btn-group" id="hover-dropdown-demo " @mouseenter="isHovered = true" @mouseleave="isHovered = false">
-                          <button type="button" class="btn btn-primary waves-effect waves-light show" style="border: 0; box-shadow: none;">
-                            <i class="fa-solid fa-ellipsis-vertical"></i>
-                          </button>
-                          <ul v-show="isHovered" class="dropdown-menu show" data-popper-placement=" bottom-end">
-                            <li>
-                              <a class="dropdown-item" href="#">Print Invoice</a>
-                            </li>
-                            <li>
-                              <a class="dropdown-item" href="#">Add New Booking</a>
-                            </li>
-                            <li><a class="dropdown-item" href="#">Audit Trail</a></li>
-                            <li>
-                              <hr class="dropdown-divider" />
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr class="border-bottom">
-                      <td>
-                        <div class="d-flex align-items-center">
-                          <i class="text-primary fa-solid fa-hotel fs-3 mr-2 mb-2"></i>
-                          <div class="card-title mb-0">
-                            <h5 class="m-0 me-2">مصطفي مدبولي</h5>
-                            <div>
-                              <i class="fa-solid fa-person m-2"></i>3
-                              <i class="fa-solid fa-child m-2"></i>2
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>14527</td>
-                      <td>
-                        <div class="text-dark">02/12/2024</div>
-                        <div>08:43:01 pm</div>
-                      </td>
-                      <td>
-                        <div class="text-dark">02/12/2024</div>
-                        <div>08:43:01 pm</div>
-                      </td>
-                      <td>202 - Suite الفندق غرفتين وصاله السعر غير شامل</td>
-                      <td>$230</td>
-                      <td>$250</td>
-                      <td class="text-danger">$20</td>
-                      <td>
-                        <div class=" btn-group" id="hover-dropdown-demo " @mouseenter="isHovered = true" @mouseleave="isHovered = false">
-                          <button type="button" class="btn btn-primary waves-effect waves-light show" style="border: 0; box-shadow: none;">
-                            <i class="fa-solid fa-ellipsis-vertical"></i>
-                          </button>
-                          <ul v-show="isHovered" class="dropdown-menu show" data-popper-placement=" bottom-end">
-                            <li>
-                              <a class="dropdown-item" href="#">Print Invoice</a>
-                            </li>
-                            <li>
-                              <a class="dropdown-item" href="#">Add New Booking</a>
-                            </li>
-                            <li><a class="dropdown-item" href="#">Audit Trail</a></li>
-                            <li>
-                              <hr class="dropdown-divider" />
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div v-else class="col-12 col-md-6 col-lg-4 mb-4 order-1 order-xl-0" @click="openOffcanvas(card)" v-for="card in reservations" :key="card.reservationNo">
-                <div class="card h-100">
-                  <div class="card-header d-flex align-items-center justify-content-between">
-                    <div class="card-title mb-0 d-flex">
-                      <i class="text-primary fa-solid fa-hotel fs-3 mr-2 mb-2"></i>
-                      <h5 class="m-0 me-2">{{ card.guestName }} </h5>
-                      <p>14527</p>
-                    </div>
-
-                    <div class="btn-group" id="hover-dropdown-demo " @mouseenter="isHovered = true" @mouseleave="isHovered = false">
-                      <div class="btn-group" id="hover-dropdown-demo " @mouseenter="isHovered = true" @mouseleave="isHovered = false">
-                        <button type="button" class="btn btn-primary waves-effect waves-light show" style="border: 0; box-shadow: none;">
-=======
           <div
             class="tab-pane fade active show"
             id="form-tabs-Reservations"
@@ -1018,7 +812,6 @@
                           class="btn btn-primary waves-effect waves-light show"
                           style="border: 0; box-shadow: none"
                         >
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
                           <i class="fa-solid fa-ellipsis-vertical"></i>
                         </button>
                         <ul
@@ -1042,32 +835,6 @@
                           </li>
                         </ul>
                       </div>
-<<<<<<< HEAD
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <div id="deliveryExceptionsChart">
-                      <div class="row d-flex align-items-center justify-content-center text-center mb-3">
-                        <div class="bg-light col-md-5">
-                          <div class="text-dark">02/12/2024</div>
-                          <div>08:43:01 pm</div>
-                        </div>
-                        <div class="bg-secondary col-md-2">
-                          <div class="text-dark">5</div>
-                          <div>Nights</div>
-                        </div>
-                        <div class="bg-light col-md-5">
-                          <div class="text-dark">02/12/2024</div>
-                          <div>08:43:01 pm</div>
-                        </div>
-                      </div>
-                      <div class="row mb-5">
-                        <div class="col-md-10">
-                          <div>Booking Date</div>
-                          <div>02/12/2024</div>
-                        </div>
-                        <div class="col-md-2">
-=======
                     </td>
                   </tr>
                   <tr class="border-bottom">
@@ -1078,17 +845,11 @@
                         ></i>
                         <div class="card-title mb-0">
                           <h5 class="m-0 me-2">مصطفي مدبولي</h5>
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
                           <div>
                             <i class="fa-solid fa-person"></i>3
                             <i class="fa-solid fa-child"></i>2
                           </div>
                         </div>
-<<<<<<< HEAD
-                        <div class="col-md-12">
-                          <div>Room / Rate Type</div>
-                          <div>202 / السعر غير شامل</div>
-=======
                       </div>
                     </td>
                     <td>14527</td>
@@ -1228,7 +989,6 @@
                         <div>
                           <i class="fa-solid fa-person"></i>3
                           <i class="fa-solid fa-child"></i>2
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
                         </div>
                       </div>
                       <div class="row">
@@ -1245,8 +1005,6 @@
               </div>
             </div>
           </div>
-<<<<<<< HEAD
-=======
 
           <!-- Arrivals  -->
           <div
@@ -1266,7 +1024,6 @@
             id="form-tabs-In-house"
             role="tabpanel"
           ></div>
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
         </template>
       </HeaderReservation>
     </div>
@@ -1355,17 +1112,10 @@ export default {
 </script>
 
 <style>
-<<<<<<< HEAD
-.active-card {
-  background-color: #f0f0f0;
-  border: 1px solid #ddd;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-=======
 .resetSearch {
   position: fixed;
   bottom: 0;
   right: 0;
   margin: 10px;
->>>>>>> a136e5176c25644b593f9c1143cf9f9da5a0bcca
 }
 </style>
