cb485f16bcf1ea482a78e74961c5a19139165739
BAssal <baslm8496@gmail.com>
2024-12-16 11:19:18 +0200

    ant-design-vue

--- components/CalenderReservation/DataTable.vue
+++ components/CalenderReservation/DataTable.vue
@@ -1,135 +1,74 @@
 <template>
-  <div class="card">
-    <h5 class="card-header">Bordered Table</h5>
-    <div class="card-body p-0">
-      <div class="table-responsive text-nowrap">
-        <table class="table">
-          <div class="date-navigation">
-            <button class="prev-days" @click="prev20Days">&lt; </button>
-            <button class="next-days" @click="next20Days"> &gt;</button>
-          </div>
-          <thead>
-            <tr>
-              <th colspan="3" style="width: 300px;">
-                <div class="demo-inline-spacing">
-                  <div class="btn-group">
-                    <button class="btn btn-primary dropdown-toggle waves-effect waves-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="javascript:void(0);">Action</a></li>
-                      <li><a class="dropdown-item" href="javascript:void(0);">Another action</a></li>
-                      <li><a class="dropdown-item" href="javascript:void(0);">Something else here</a></li>
-                      <li>
-                        <hr class="dropdown-divider" />
-                      </li>
-                      <li><a class="dropdown-item" href="javascript:void(0);">Separated link</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </th>
-
-              <th v-for="date in daysRange" :key="date">
-                <div>
-                  <div>{{ date.weekday }}</div>
-                  <div>{{ date.dayMonth }}</div>
-                </div>
-              </th>
-            </tr>
-          </thead>
-
-        </table>
-      </div>
-    </div>
-  </div>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in desserts" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.calories }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
-
 <script>
+
 export default {
   data ()
   {
     return {
-      startDate: new Date(),
-    };
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+        },
+      ],
+    }
   },
-  computed: {
-    daysRange ()
-    {
-      const range = [];
-      for (let i = 0; i < 20; i++) {
-        const currentDate = new Date(this.startDate.getTime() + i * 24 * 60 * 60 * 1000);
-        range.push({
-          weekday: this.formatWeekday(currentDate),
-          dayMonth: this.formatDayMonth(currentDate),
-        });
-      }
-      return range;
-    },
-  },
-  methods: {
-    formatWeekday (date)
-    {
-      const options = { weekday: 'short' };
-      return date.toLocaleDateString(undefined, options);
-    },
-    formatDayMonth (date)
-    {
-      const options = { day: 'numeric', month: 'short' };
-      return date.toLocaleDateString(undefined, options);
-    },
-    next20Days ()
-    {
-      this.startDate = new Date(this.startDate.getTime() + 20 * 24 * 60 * 60 * 1000);
-    },
-    prev20Days ()
-    {
-      this.startDate = new Date(this.startDate.getTime() - 20 * 24 * 60 * 60 * 1000);
-    },
-  },
-};
+}
 </script>
-
-<style>
-.date-navigation button {
-  width: 40px;
-  height: 40px;
-  background: red;
-  border: 0;
-  outline: 0;
-  border-radius: 50%;
-}
-
-.next-days {
-  position: absolute;
-  top: 90px;
-  right: -10px;
-}
-
-.prev-days {
-  position: absolute;
-  top: 90px;
-  left: 130px;
-}
-
-
-.dates-container {
-  display: flex;
-  gap: 5px;
-  flex-wrap: wrap;
-}
-
-th>div {
-  text-align: center;
-}
-
-.table,
-th,
-td {
-  border: 1px solid #5d596c;
-  border-collapse: collapse;
-}
-
-.table-bordered> :not(caption)>*>* {
-  border-width: 3px;
-}
-</style>
