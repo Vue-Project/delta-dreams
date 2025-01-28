560bc8775a902735124efd4ce8843627c9078fdb
samamahfouz <samamahfoz43@gmail.com>
2025-01-28 14:16:54 +0200

    delete comment

--- components/CalenderReservation/SelectedEventSidebar.vue
+++ components/CalenderReservation/SelectedEventSidebar.vue
@@ -6,7 +6,6 @@
         <template v-if="selectedEvent">
           <h6>
             <i class="fa-solid fa-user pr-2 text-primary fs-3 mb-2"></i>
-            <!-- <i class="fa-solid fa-person pr-2 text-primary fs-3 mb-2"></i> -->
             {{ selectedEvent.user?.name }}
           </h6>
           <div>
