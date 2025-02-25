b47557b2570bebcc7ab3c5ebcfabbbfeb419a746
BAssal <baslm8496@gmail.com>
2025-02-25 17:47:21 +0200

    Enhance FilterCalendar and HeaderCalendar with Advanced Filtering Capabilities
    - Add dynamic building, rate type, and project filtering in HeaderCalendar
    - Implement dropdown menus with checkbox selection for buildings, rate types, and projects
    - Add 'Apply Filters' button to send selected filter criteria to server
    - Refactor FilterCalendar component to leverage new filtering methods
    - Update CSS styles for dropdown menus and filter interactions

--- components/CalenderReservation/FilterCalendar.vue
+++ components/CalenderReservation/FilterCalendar.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <HeaderCalender ref="headerCalender" @date-selected="handleDateSelected" :statistics="statistics" />
-
-    <div class="dropdown">
+    <HeaderCalender
+      ref="headerCalender"
+      @date-selected="handleDateSelected"
+      @show-all-resources="$emit('show-all-resources')"
+      @show-building-resources="$emit('show-building-resources', $event)"
+      :statistics="statistics"
+      :buildingNames="buildingNames"
+    />
+    <!-- <div class="dropdown">
         <button class="btn btn-primary dropdown-toggle btn-block col-lg-12 col-sm-7" type="button" id="dropdownFilterButton" data-bs-toggle="dropdown" aria-expanded="false">
           <i class="fa-solid fa-filter pe-2"></i>Filter Resources
       </button>
@@ -23,7 +29,7 @@
           </a>
         </li>
       </ul>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -45,80 +51,16 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      selectedBuildings: [], // Holds the selected buildings
-      selectAll: false, // Controls the "Show All" checkbox
-    };
-  },
   methods: {
     handleDateSelected(selectedDate) {
-      // Forward the selected date to the MainComponent
       this.$emit("date-selected", selectedDate);
-    },
-    // Toggle "Show All" checkbox
-    toggleSelectAll() {
-      this.selectAll = !this.selectAll;
-      if (this.selectAll) {
-        // If "Show All" is checked, clear selected buildings and emit "show-all-resources"
-        this.selectedBuildings = [];
-        this.$emit("show-all-resources");
-      } else {
-        // If "Show All" is unchecked, emit the current selected buildings
-        this.$emit("show-building-resources", this.selectedBuildings);
-      }
-    },
-    // Toggle individual building selection
-    toggleBuilding(building) {
-      if (this.selectedBuildings.includes(building)) {
-        // If the building is already selected, remove it
-        this.selectedBuildings = this.selectedBuildings.filter((b) => b !== building);
-      } else {
-        // If the building is not selected, add it
-        this.selectedBuildings.push(building);
-      }
-
-      // Update "Show All" checkbox state
-      this.selectAll = this.selectedBuildings.length === 0;
-
-      // Emit the selected buildings
-      this.$emit("show-building-resources", this.selectedBuildings);
-    },
-  },
-  watch: {
-    // Watch for changes in selectedBuildings to update "Show All" checkbox
-    selectedBuildings(newVal) {
-      if (newVal.length === 0) {
-        this.selectAll = true; // If no buildings are selected, check "Show All"
-      } else {
-        this.selectAll = false; // If any buildings are selected, uncheck "Show All"
-      }
     },
   },
 };
 </script>
 
 <style scoped>
-.dropdown {
-  position: absolute;
-  left: 0px;
-  width: 280px;
-  max-width: 280px;
-  margin: 0 9px ;
-}
 
-.dropdown-menu {
-  padding: 10px;
-  width: 280px;
-}
 
-.dropdown-item {
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-}
 
-.form-check-input {
-  margin-right: 8px;
-}
 </style>
--- components/CalenderReservation/HeaderCalender.vue
+++ components/CalenderReservation/HeaderCalender.vue
@@ -5,36 +5,102 @@
       <input type="text" class="form-control flatpickr-input mb-sm-2" placeholder="YYYY-MM-DD" id="flatpickr-date-04" ref="datePicker4" aria-label="input for date" />
       <i class="fa-solid fa-calendar-days date-icon"></i>
     </div>
-    <div class="col-lg-6 col-12">
-      <div class="d-flex items-center gap-4">
+
+    <div class="col-lg-4 col-12">
+      <div v-if="hasSelections">
+        <button class="btn btn-success float-end" @click="applyFilters">
+          Apply Filters
+        </button>
+      </div>
+    </div>
+    <!-- <div class="d-flex items-center gap-4">
         <div v-for="(count, label) in statistics" :key="label">
           <span>{{ label }}</span>
           <span class="statistics-count">
             {{ count }}
           </span>
         </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-12">
+      </div> -->
+    <div class="col-lg-6 col-12">
       <div class="d-flex items-center justify-content-end gap-4">
         <div class="w-100">
           <div class="row">
-            <div class="col-md-6">
-              <select class="form-select">
-                <option disabled value="">select</option>
-                <option v-for="(type, index) in getRateTypes" :key="index" :value="index">
-                  {{ type }}
-                </option>
-              </select>
+            <div class="col-md-4">
+              <div class="dropdown">
+                <button class="btn  btn-primary  dropdown-toggle w-100" type="button" id="buildingsDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fa-solid fa-filter pe-2"></i>Filter Buildings
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="buildingsDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#" @click.prevent="toggleSelectAllBuildings">
+                      <input type="checkbox" v-model="selectAllBuildings" class="form-check-input me-2" />
+                      <span>Show All</span>
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li v-for="building in buildingNames" :key="building">
+                    <a class="dropdown-item" href="#" @click.prevent="toggleBuilding(building)">
+                      <input type="checkbox" v-model="selectedBuildings" :value="building" class="form-check-input me-2" />
+                      <span>{{ building }}</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div class="col-md-6">
-              <select class="form-select">
-                <option disabled value="">select</option>
-                <option v-for="project in getProjects" :key="project.id" :value="project.id">
-                  {{ project.name }}
-                </option>
-              </select>
+            <div class="col-md-4">
+              <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle w-100" type="button" id="rateTypesDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fa-solid fa-filter pe-2"></i>Filter Rate Types
+                  <span v-if="selectedRateTypes.length" class="badge bg-light text-dark ms-1">
+                    {{ selectedRateTypes.length }}
+                  </span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="rateTypesDropdown">
+                  <!-- <li>
+                    <a class="dropdown-item" href="#" @click.prevent="toggleSelectAllRateTypes">
+                      <input type="checkbox" v-model="selectAllRateTypes" class="form-check-input me-2">
+                      <span>Show All</span>
+                    </a>
+                  </li> -->
+                  <!-- <li><hr class="dropdown-divider"></li> -->
+                  <li v-for="(type, index) in getRateTypes" :key="index">
+                    <a class="dropdown-item" href="#" @click.prevent="toggleRateType(index)">
+                      <input type="checkbox" :value="index" v-model="selectedRateTypes" class="form-check-input me-2">
+                      <span>{{ type }}</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
+            <div class="col-md-4">
+              <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle w-100" type="button" id="projectsDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fa-solid fa-filter pe-2"></i>Filter Projects
+                  <span v-if="selectedProjects.length" class="badge bg-light text-dark ms-1">
+                    {{ selectedProjects.length }}
+                  </span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="projectsDropdown">
+                  <!-- <li>
+                    <a class="dropdown-item" href="#" @click.prevent="toggleSelectAllProjects">
+                      <input type="checkbox" v-model="selectAllProjects" class="form-check-input me-2">
+                      <span>Show All</span>
+                    </a>
+                  </li> -->
+                  <!-- <li><hr class="dropdown-divider"></li> -->
+                  <li v-for="project in getProjects" :key="project.id">
+                    <a class="dropdown-item" href="#" @click.prevent="toggleProject(project.id)">
+                      <input type="checkbox" :value="project.id" v-model="selectedProjects" class="form-check-input me-2">
+                      <span>{{ project.name }}</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+
           </div>
 
 
@@ -144,6 +210,8 @@
 <script>
 import flatpickrMixin from "../Mixin/flatpickrMixin";
 import { mapState, mapGetters } from 'vuex';
+import _ from 'lodash';
+import { handleSubmissionError, showSuccessAlert } from "../../Api/MassageValidation/alertUtilities";
 
 export default {
   name: "HeaderCalender",
@@ -156,6 +224,14 @@ export default {
       sidebarVisible: false,
       isOn: false,
       isSidebarOpen: false,
+      selectedRateTypes: [],
+      selectedProjects: [],
+      showRateTypesDropdown: false,
+      showProjectsDropdown: false,
+      selectAllRateTypes: false,
+      selectAllProjects: false,
+      selectedBuildings: [],
+      selectAllBuildings: true,
     };
   },
 
@@ -179,6 +255,104 @@ export default {
         this.$refs.datePicker4._flatpickr.setDate(date); // Update Flatpickr with the new date
       }
     },
+    toggleRateTypesDropdown ()
+    {
+      this.showRateTypesDropdown = !this.showRateTypesDropdown;
+      this.showProjectsDropdown = false;
+    },
+    toggleProjectsDropdown ()
+    {
+      this.showProjectsDropdown = !this.showProjectsDropdown;
+      this.showRateTypesDropdown = false;
+    },
+    toggleSelectAllRateTypes ()
+    {
+      this.selectAllRateTypes = !this.selectAllRateTypes;
+      if (this.selectAllRateTypes) {
+        this.selectedRateTypes = this.getRateTypes.map((_, index) => index);
+      } else {
+        this.selectedRateTypes = [];
+      }
+    },
+    toggleSelectAllProjects ()
+    {
+      this.selectAllProjects = !this.selectAllProjects;
+      if (this.selectAllProjects) {
+        this.selectedProjects = this.getProjects.map(project => project.id);
+      } else {
+        this.selectedProjects = [];
+      }
+    },
+    async toggleRateType (index)
+    {
+      const idx = this.selectedRateTypes.indexOf(index);
+      if (idx === -1) {
+        this.selectedRateTypes.push(index);
+      } else {
+        this.selectedRateTypes.splice(idx, 1);
+      }
+      this.selectAllRateTypes = this.selectedRateTypes.length === this.getRateTypes.length;
+      await this.sendSelectionsToServer();
+    },
+    async toggleProject (projectId)
+    {
+      const idx = this.selectedProjects.indexOf(projectId);
+      if (idx === -1) {
+        this.selectedProjects.push(projectId);
+      } else {
+        this.selectedProjects.splice(idx, 1);
+      }
+      this.selectAllProjects = this.selectedProjects.length === this.getProjects.length;
+      await this.sendSelectionsToServer();
+    },
+    toggleSelectAllBuildings ()
+    {
+      this.selectAllBuildings = !this.selectAllBuildings;
+      if (this.selectAllBuildings) {
+        this.selectedBuildings = [];
+        this.$emit("show-all-resources");
+      } else {
+        this.$emit("show-building-resources", this.selectedBuildings);
+      }
+    },
+    async toggleBuilding (building)
+    {
+      if (this.selectedBuildings.includes(building)) {
+        this.selectedBuildings = this.selectedBuildings.filter((b) => b !== building);
+      } else {
+        this.selectedBuildings.push(building);
+      }
+      this.selectAllBuildings = this.selectedBuildings.length === 0;
+      this.$emit("show-building-resources", this.selectedBuildings);
+      await this.sendSelectionsToServer();
+    },
+    async sendSelectionsToServer ()
+    {
+
+      try {
+        const payload = {
+          project_id: this.selectedProjects,
+          rate_types: this.selectedRateTypes,
+        };
+        console.log(payload);
+
+        // await showSuccessAlert(
+        //     "Reservation cancelled successfully!", // Custom message
+
+        //   );
+
+      } catch (error) {
+        // handleSubmissionError(
+        //     error,
+        //     "Failed to cancel reservation" // Updated error message
+        //   );
+        }
+    },
+    async applyFilters ()
+    {
+      await this.sendSelectionsToServer();
+
+    },
   },
 
   mounted ()
@@ -199,6 +373,10 @@ export default {
       'getProjects',
       'getProjects'
     ]),
+    hasSelections ()
+    {
+      return this.selectedRateTypes.length > 0 || this.selectedProjects.length > 0;
+    }
 
 
   },
@@ -209,6 +387,41 @@ export default {
       type: Object,
       required: true,
     },
+    buildingNames: {
+      type: Array,
+      required: true,
+    },
   },
+  // watch: {
+  //   selectedBuildings(newVal) {
+  //     if (newVal.length === 0) {
+  //       this.selectAllBuildings = true;
+  //     } else {
+  //       this.selectAllBuildings = false;
+  //     }
+  //   },
+  //   selectedRateTypes: {
+  //     handler: _.debounce(async function(newVal) {
+  //       await this.sendSelectionsToServer();
+  //     }, 500),
+  //     deep: true
+  //   },
+  //   selectedProjects: {
+  //     handler: _.debounce(async function(newVal) {
+  //       await this.sendSelectionsToServer();
+  //     }, 500),
+  //     deep: true
+  //   },
+  //   selectedBuildings: {
+  //     handler: _.debounce(async function(newVal) {
+  //       await this.sendSelectionsToServer();
+  //     }, 500),
+  //     deep: true
+  //   }
+  // },
 };
 </script>
+
+<style scoped>
+
+</style>
