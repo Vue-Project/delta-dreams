b706dbe9e71e53a58383b75953d903e6210c438b
abobakr_zayyan <abobakr.zian@systemira.net>
2025-03-10 14:55:50 +0200

    Middleware & Packages: Add js-cookie for persistent URL parameter storage

--- middleware/permissionQuery.js
+++ middleware/permissionQuery.js
@@ -1,36 +1,62 @@
-export default function ({ route, redirect, store }) {
-  // Extract code and type from the current route's query
-  const { code, type } = route.query
+// export default function ({ route, redirect, store }) {
+//   // Extract code and type from the current route's query
+//   const { code, type } = route.query
 
-  // Check if we have new parameters in the URL
-  if (code || type) {
-    // If new parameters exist, update the store
+//   // Check if we have new parameters in the URL
+//   if (code || type) {
+//     // If new parameters exist, update the store
+//     store.commit('setParams', {
+//       code: code || store.state.code,
+//       type: type || store.state.type,
+//       // Store when these params were last updated
+//       // lastUpdated: new Date().toISOString()
+//     })
+//   }
+
+//   // Get all values from the store
+//   const { code: storedCode, type: storedType } = store.state
+
+//   // Only add parameters to routes that don't already have them
+//   if ((!code && storedCode) || (!type && storedType)) {
+//     // Create new query with stored values for missing parameters
+//     const newQuery = { ...route.query }
+
+//     // Only add parameters that aren't already in the URL
+//     if (!code && storedCode) newQuery.code = storedCode
+//     if (!type && storedType) newQuery.type = storedType
+
+//     // Redirect if we need to add parameters
+//     if (JSON.stringify(route.query) !== JSON.stringify(newQuery)) {
+//       return redirect({
+//         path: route.path,
+//         query: newQuery
+//       })
+//     }
+//   }
+// }
+export default function ({ route, redirect, store }) {
+  const { code, type } = route.query;
+
+  // Update store with URL params or fallback to cookies
+  const storedCode = code || Cookies.get('code');
+  const storedType = type || Cookies.get('type');
+
+  if (storedCode || storedType) {
     store.commit('setParams', {
-      code: code || store.state.code,
-      type: type || store.state.type,
-      // Store when these params were last updated
-      // lastUpdated: new Date().toISOString()
-    })
+      code: storedCode,
+      type: storedType,
+    });
+    // Save to cookies for persistence
+    Cookies.set('code', storedCode, { expires: 7 });
+    Cookies.set('type', storedType, { expires: 7 });
   }
 
-  // Get all values from the store
-  const { code: storedCode, type: storedType } = store.state
+  // Redirect if URL is missing params but store/cookies have them
+  const expectedQuery = { ...route.query };
+  if (!code && storedCode) expectedQuery.code = storedCode;
+  if (!type && storedType) expectedQuery.type = storedType;
 
-  // Only add parameters to routes that don't already have them
-  if ((!code && storedCode) || (!type && storedType)) {
-    // Create new query with stored values for missing parameters
-    const newQuery = { ...route.query }
-
-    // Only add parameters that aren't already in the URL
-    if (!code && storedCode) newQuery.code = storedCode
-    if (!type && storedType) newQuery.type = storedType
-
-    // Redirect if we need to add parameters
-    if (JSON.stringify(route.query) !== JSON.stringify(newQuery)) {
-      return redirect({
-        path: route.path,
-        query: newQuery
-      })
-    }
+  if (JSON.stringify(route.query) !== JSON.stringify(expectedQuery)) {
+    return redirect({ path: route.path, query: expectedQuery });
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div class="row p-3">
 
     <div class="col-md-12">
-      <!-- <a href="http://localhost:3000?code=mR6Q8cgZHkSFtTfv&type=admin" class="menu-link">
+      <a href="http://localhost:3000?code=mR6Q8cgZHkSFtTfv&type=admin" class="menu-link">
         <div data-i18n="Email">Reservation</div>
-      </a> -->
+      </a>
       <BookingCalendar />
     </div>
   </div>
