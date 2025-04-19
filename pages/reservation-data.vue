585ed8a4a2c23806eceffd0c657d203743a4756b
samamahfouz <samamahfoz43@gmail.com>
2025-04-19 15:08:18 +0200

    reservation-data.vue : import room-status in reservation

---
+++ pages/reservation-data.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row px-3">
+      <div class="col-md-12 py-4">
+        <room-status />
+      </div>
+    </div>
+  </template>
+  <style scoped></style>
+  
+  <script>
+  import roomStatus from '../components/RoomView/room-status.vue';
+  
+  export default {
+    name: "RoomViewPage",
+    layout: "main",
+    components: {
+        roomStatus
+  
+    }
+  };
+  
+  </script>
+  
