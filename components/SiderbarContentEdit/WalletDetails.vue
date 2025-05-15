ce4d8faf066a8bb424699c4c3868aec191a2b25e
abobakr_zayyan <abobakr.zian@systemira.net>
2025-05-15 16:24:02 +0300

    fix(WalletDetails): update reservation display to show name instead of ID

--- components/SiderbarContentEdit/WalletDetails.vue
+++ components/SiderbarContentEdit/WalletDetails.vue
@@ -82,7 +82,7 @@
                                             <div class="modal-body px-4 py-3">
                                                 <div class="mb-4">
                                                     <h6 class="fw-semibold">Reservation Name:</h6>
-                                                    <p class="mb-0 text-muted">{{ reservationData?.id }}</p>
+                                                    <p class="mb-0 text-muted">#{{ reservationData?.name }}</p>
                                                 </div>
                                                 <!-- <div class="mb-4">
                                                     <h6 class="fw-semibold">Wallet Number:</h6>
