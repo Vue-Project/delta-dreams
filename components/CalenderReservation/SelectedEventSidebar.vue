d317f3688d797174c0fd33fdd28d04c423b1d86e
BAssal <baslm8496@gmail.com>
2025-05-21 17:42:59 +0300

    fix(BookingCalendar, SelectedEventSidebar): add insurance refund and remaining fields to event details

--- components/CalenderReservation/SelectedEventSidebar.vue
+++ components/CalenderReservation/SelectedEventSidebar.vue
@@ -239,6 +239,10 @@
                         <dd class="col-6 text-end mb-0">{{ selectedEvent.total_service }} EGP</dd>
                         <dt class="col-6 fw-normal text-heading">Insurance</dt>
                         <dd class="col-6 text-end mb-0">{{ selectedEvent.insurance || 0 }} EGP</dd>
+                        <dt class="col-6 fw-normal text-heading">Insurance Refund</dt>
+                        <dd class="col-6 text-end mb-0">{{ selectedEvent.insurance_refund }} EGP</dd>
+                        <dt class="col-6 fw-normal text-heading">Insurance Remaining</dt>
+                        <dd class="col-6 text-end mb-0">{{ selectedEvent.insurance_remaining }} EGP</dd>
 
                         <dt class="col-6 fw-normal text-heading">Total</dt>
                         <dd class="col-6 text-end mb-0">{{ selectedEvent.total }} EGP</dd>
