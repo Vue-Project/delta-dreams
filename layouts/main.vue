d245de9962681d8657deb173e112a9c5cca7cb3e
abobakr_zayyan <abobakr.zian@systemira.net>
2025-06-15 16:13:11 +0300

    fix(main): adjust indentation and formatting for improved readability

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
-    <!-- <TheHeader /> -->
-    <HeaderNav></HeaderNav>
-    <NotificationAlert />
-    <PusherStatus />
-    <Nuxt />
-  </div>
+    <div>
+        <!-- <TheHeader /> -->
+        <HeaderNav></HeaderNav>
+        <NotificationAlert />
+        <!-- <PusherStatus /> -->
+        <Nuxt />
+    </div>
 </template>
 
 <script>
-import { CursorFx } from "@luxdamore/vue-cursor-fx";
-import TheHeader from "../components/layout/TheHeader.vue";
-import HeaderNav from "../components/layout/HeaderNav.vue";
-import NotificationAlert from "../components/NotificationAlert.vue";
-import PusherStatus from "../components/PusherStatus.vue";
-// import 'themify-icons/css/themify-icons.css';
+    import { CursorFx } from '@luxdamore/vue-cursor-fx';
+    import TheHeader from '../components/layout/TheHeader.vue';
+    import HeaderNav from '../components/layout/HeaderNav.vue';
+    import NotificationAlert from '../components/NotificationAlert.vue';
+    import PusherStatus from '../components/PusherStatus.vue';
+    // import 'themify-icons/css/themify-icons.css';
 
-export default {
-  components: { CursorFx, TheHeader, HeaderNav, NotificationAlert, PusherStatus },
-};
+    export default {
+        components: { CursorFx, TheHeader, HeaderNav, NotificationAlert, PusherStatus },
+    };
 </script>
 
 <style src="@luxdamore/vue-cursor-fx/dist/CursorFx.css"></style>
